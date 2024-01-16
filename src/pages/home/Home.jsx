855ee618e2dde98e53d44bf202dc7cc93a55ffeb
Rifazul <rifazul50@gmail.com>
2024-01-16 09:38:07 +0600

    insert the service heading

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
+import OurService from "./OurService/OurService";
+
 const Home = () => {
-    return (
-        <div>
-            <h3 className="text-xl mt-10">This is home page</h3>
-        </div>
-    );
+  return (
+    <div>
+      <h3 className="text-xl mt-10">This is home page</h3>
+
+      <OurService></OurService>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/pages/home/OurService/OurService.jsx
@@ -0,0 +1,12 @@
+const OurService = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-center text-[#248479] font-bold">
+        {" "}
+        Our Service{" "}
+      </h1>
+    </div>
+  );
+};
+
+export default OurService;
