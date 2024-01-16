b863052724db34c0a4e87697ff9a078b381371a5
ashrafulrifaz <developerashrafulislamrifaz@gmail.com>
2024-01-16 14:56:57 +0600

    added slider to banner section

--- src/pages/components/Banner/Banner.jsx
+++ src/pages/components/Banner/Banner.jsx
@@ -1,15 +1,55 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Autoplay, Pagination } from 'swiper/modules';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
 const Banner = () => {
+    const { isPending, error, data: banner } = useQuery({
+        queryKey: ['banner_info'],
+        queryFn: async() => {
+            const res = await axios.get('banner_info.json')
+            return res.data
+        }
+    })
+
+
     return (
         <div className="banner">
-            <div className="space-y-4">
-                <h2>Elevate Your Workforce Management</h2>
-                <p>Experience enhanced efficiency and engagement with our comprehensive HR solutions. Streamline processes and elevate your workplace experience today.</p>
-                <button className="button">Explore Solutions</button>
-            </div>
-            <div>
-                <img src="https://i.ibb.co/W0xN5rn/banner-2.jpg" alt="" />
-            </div>
+            <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+                }}
+                pagination={{
+                clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination]}
+                className="mySwiper"
+            >
+                {
+                    banner?.map((ban, idx) => (
+                        <SwiperSlide key={idx}>
+                            <div className="grid grid-cols-2 items-center">
+                                <div className="space-y-4">
+                                    <span>Hi there</span>
+                                    <h2>{ban?.title}</h2>
+                                    <p>{ban?.description}</p>
+                                    <button className="button">{ban?.button}</button>
+                                </div>
+                                <div>
+                                    <img src={ban?.img} alt="" />
+                                </div>     
+                            </div>   
+                        </SwiperSlide>
+                    ))
+                }
+            </Swiper>
+            
         </div>
     );
 };
