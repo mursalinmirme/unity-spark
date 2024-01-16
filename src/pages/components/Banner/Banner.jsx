f714cbf390fa3611e407c771862634e794c7ee9a
ashrafulrifaz <developerashrafulislamrifaz@gmail.com>
2024-01-16 13:29:53 +0600

    added error page and designed banner section

---
+++ src/pages/components/Banner/Banner.jsx
@@ -0,0 +1,17 @@
+
+const Banner = () => {
+    return (
+        <div className="banner">
+            <div className="space-y-4">
+                <h2>Elevate Your Workforce Management</h2>
+                <p>Experience enhanced efficiency and engagement with our comprehensive HR solutions. Streamline processes and elevate your workplace experience today.</p>
+                <button className="button">Explore Solutions</button>
+            </div>
+            <div>
+                <img src="https://i.ibb.co/W0xN5rn/banner-2.jpg" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,9 @@
+import Banner from "../components/Banner/Banner";
+
 const Home = () => {
     return (
         <div>
-            <h3 className="text-xl mt-10">This is home page</h3>
+            <Banner></Banner>
         </div>
     );
 };
