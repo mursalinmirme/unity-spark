275621992a8bffae1a7c226532bc6669fc323bb0
Rifazul <rifazul50@gmail.com>
2024-01-16 18:50:00 +0600

    insert the our service card

--- src/pages/home/OurService/OurService.jsx
+++ src/pages/home/OurService/OurService.jsx
@@ -1,10 +1,28 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import ServiceCard from "./ServiceCard";
+
 const OurService = () => {
+  const [services, serServices] = useState([]);
+
+  useEffect(() => {
+    fetch("/service.json")
+      .then((res) => res.json())
+      .then((data) => serServices(data));
+  }, []);
+
   return (
     <div>
       <h1 className="text-3xl text-center text-[#248479] font-bold">
         {" "}
         Our Service{" "}
       </h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 lg:px-24 my-16 ">
+        {services?.map((service) => (
+          <ServiceCard key={service.id} service={service}></ServiceCard>
+        ))}
+      </div>
     </div>
   );
 };
