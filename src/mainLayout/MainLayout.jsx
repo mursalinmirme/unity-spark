b4ada9f90deffbc99c409f837e6ea3e415c6a4db
mursalinmirme <mursalinmir02@gmail.com>
2024-01-17 02:03:08 +0600

    I have removed an extra div from mainLoyout components

--- src/mainLayout/MainLayout.jsx
+++ src/mainLayout/MainLayout.jsx
@@ -6,10 +6,7 @@ const MainLayout = () => {
     return (
         <div className="max-w-screen-2xl mx-auto font-poppins">
             <Navbar></Navbar>
-            {/* this div is used for single time after I will remove the div */}
-            <div className="min-h-[70vh]">
                <Outlet></Outlet>
-            </div>
             <Footer></Footer>
         </div>
     );
