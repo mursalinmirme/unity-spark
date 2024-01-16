d5388f656d563307abd627cefe405221af232e1a
mdabdurrahman07 <jamilrahman2001@gmail.com>
2024-01-16 15:49:40 +0600

    jamil testing

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -3,6 +3,7 @@ const Signup = () => {
     return (
         <div>
             <h3 className="text-xl mt-10">this is sign up page</h3>
+            <h1>this is jamil</h1>
         </div>
     );
 };
