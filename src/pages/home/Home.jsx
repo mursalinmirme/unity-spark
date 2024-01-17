ae5c152afc953ce492b8af39bf6dfa3c5ea13a64
ashrafulrifaz <developerashrafulislamrifaz@gmail.com>
2024-01-17 15:57:02 +0600

    try to solve conflict

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import Banner from "../components/Banner/Banner";
 import Counter from "../components/Counter/Counter";
 import Goals from "../components/Goals/Goals";
+import OurService from "./OurService/OurService";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Counter></Counter>
+            <OurService></OurService>
             <Goals></Goals>
         </div>
     );
--- src/pages/home/OurService/OurService.jsx
+++ src/pages/home/OurService/OurService.jsx
@@ -15,7 +15,6 @@ const OurService = () => {
     <div className="py-10">
       <h2 className="text-3xl text-center text-[#248479] font-bold mt-10">Empowering Your Journey</h2>
       <p className="text-center px-64 mt-3 font-inter font-medium">Our solutions are useful for driving innovation, fostering collaboration, and achieving success in your business endeavors.</p>
-      <p className="text-center px-64 mt-3 font-inter font-medium">Our solutions are useful for driving innovation, fostering collaboration, and achieving success in your business endeavors.</p>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6 lg:px-24 mb-16 mt-8 p-4">
         {services?.map((service) => (
           <ServiceCard key={service.id} service={service}></ServiceCard>
