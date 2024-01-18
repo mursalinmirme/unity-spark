877dd7223ca496f5af9b3508d81af862c236390f
ashrafulrifaz <developerashrafulislamrifaz@gmail.com>
2024-01-18 12:15:55 +0600

    changed error page image

--- src/pages/components/HomeComponents/Banner/Banner.jsx
+++ src/pages/components/HomeComponents/Banner/Banner.jsx
@@ -36,6 +36,7 @@ const Banner = () => {
                         <SwiperSlide key={idx}>
                             <div className="grid grid-cols-2 items-center">
                                 <div className="space-y-5">
+                                    <img src={'https://i.ibb.co/sJZrRTS/wait.gif'} className='w-10 mb-5' alt="" />
                                     <span>Hi there</span>
                                     <h2>{ban?.title}</h2>
                                     <p>{ban?.description}</p>
--- src/pages/components/HomeComponents/JobPost/JobPost.jsx
+++ src/pages/components/HomeComponents/JobPost/JobPost.jsx
@@ -14,16 +14,12 @@ const JobPost = () => {
       <h2 className="text-4xl text-center text-[#1E1E1E] font-bold mb-1 mt-7">
         {" "}
         Our Recent Job Post{" "}
-<<<<<<< HEAD:src/pages/home/JobPost/JobPost.jsx
       </h2>
       <p className="text-center lg:w-[696px] mx-auto text-[#1E1E1E] mt-2 mb-8">
         {" "}
         Our solutions are useful for driving innovation, fostering
         collaboration, and achieving success in your business endeavors.
       </p>
-=======
-      </h1>
->>>>>>> bd3e9eb26df4fd6dd0c36ecec6b0b255c654ad6f:src/pages/components/HomeComponents/JobPost/JobPost.jsx
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:w-11/12 mx-auto gap-7 px-2 md:px-3 mb-10">
         {jobPosts.map((jobPost) => (
