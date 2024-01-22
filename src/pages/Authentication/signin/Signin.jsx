7419bf34e7a2e14d17991b31443984be52690900
mursalinmirme <mursalinmir02@gmail.com>
2024-01-23 01:16:07 +0600

    Authentication users data saved into database system complete

--- src/pages/Authentication/signin/Signin.jsx
+++ src/pages/Authentication/signin/Signin.jsx
@@ -8,26 +8,46 @@ import toast from "react-hot-toast";
 import Social_Media from "../../components/Share/Social_Media/Social_Media";
 import { IoEyeOutline } from "react-icons/io5";
 import { FaEyeSlash } from "react-icons/fa";
+import axios from "axios";
 const Signin = () => {
   const { userSignIn } = useContext(AuthContext);
   const [open, setOpen] = useState(false);
+  const [signInLoading, setSignInLoading] = useState(false);
   const navigate = useNavigate();
 
   const { register, handleSubmit, reset } = useForm();
   const onSubmit = (data) => {
+    setSignInLoading(true);
     // User Login Method
     userSignIn(data?.email, data?.password)
-      .then((result) => {
-        console.log(result.user);
-        toast.success("User Login Successfully");
-
-        navigate("/");
+      .then((res) => {
+        axios.get(`http://localhost:5000/user-role?email=${res?.user?.email}`)
+        .then((resp) => {
+           if(resp.data.role === 'user') {
+            reset();
+            setSignInLoading(false);
+            navigate("/dashboard");
+            toast.success("Login Successfully");
+           }
+           if(resp.data.role === 'admin') {
+            reset();
+            setSignInLoading(false);
+            navigate("/dashboard"); //it will update after complete the admin dashboard
+            toast.success("Login Successfully");
+           }          
+        })
+        .catch((err) => {
+          setSignInLoading(false);
+          toast.error(err.message);
+        })
+        
       })
       .catch((error) => {
         toast.error(error.message);
+        setSignInLoading(false);
       });
 
-    reset();
+
   };
 
   return (
@@ -94,13 +114,13 @@ const Signin = () => {
             </label>
             {/* password field */}
             <div className="form-control w-full py-3">
-              <button>Sign In</button>
+            <button className="text-base flex justify-center items-center">{signInLoading ? <span className="loading loading-spinner loading-md"></span> : "Sign Up" }</button>
             </div>
           </form>
           <h1 className="text-center text-gray-700 font-medium">Or</h1>
 
           {/** Social Media Login System */}
-          <Social_Media> </Social_Media>
+          <Social_Media setSignInLoading={setSignInLoading}> </Social_Media>
 
           <div className="text-center py-3">
             <p className="text-sm text-gray-700 font-medium">
