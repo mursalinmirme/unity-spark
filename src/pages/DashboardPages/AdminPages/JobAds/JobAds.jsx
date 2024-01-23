4a723b698b36f91ee25a264d332f5f5ce6a791e6
nisakib07 <websakib07@gmail.com>
2024-01-23 12:55:32 +0600

    [Dashboard : Job Ads]- Tabs made

--- src/pages/DashboardPages/AdminPages/JobAds/JobAds.jsx
+++ src/pages/DashboardPages/AdminPages/JobAds/JobAds.jsx
@@ -1,11 +1,51 @@
-import maintainance from '../../../../assets/images/maintainance.png'
+import { useState } from "react";
+import ManageAds from "../../../components/Dashboard/JobAds/ManageAds";
+import ManageApplications from "../../../components/Dashboard/JobAds/ManageApplications";
+import ManageApplicant from "../../../components/Dashboard/JobAds/ManageApplicant";
 
 const JobAds = () => {
-    return (
-        <div>
-            <img src={maintainance} className='w-3/4 mx-auto' alt="" />            
-        </div>
-    );
+  const [isActive, setIsActive] = useState(0);
+
+  const handleJobAdsTab = (id) => {
+    setIsActive(id);
+  };
+
+  const tabs = [
+    {
+      name: "Manage Job Ads",
+      id: 0,
+    },
+    {
+      name: "Manage Applications",
+      id: 1,
+    },
+    {
+      name: "Manage Applicant",
+      id: 2,
+    },
+  ];
+
+  return (
+    <div>
+      <div className="flex justify-between bg-[#e3e2f5] rounded-lg p-3">
+        {tabs?.map((tab) => (
+          <a
+            key={tab.id}
+            className={`${
+              isActive === tab.id
+                ? "font-medium text-white bg-primary px-5 py-3 rounded-lg"
+                : "bg-transparent text-primary px-5 py-3 font-semibold cursor-pointer"
+            }`}
+            onClick={() => handleJobAdsTab(tab.id)}>
+            {tab.name}
+          </a>
+        ))}
+      </div>
+      {isActive === 0 && <ManageAds />}
+      {isActive === 1 && <ManageApplications />}
+      {isActive === 2 && <ManageApplicant />}
+    </div>
+  );
 };
 
 export default JobAds;
---
+++ src/pages/components/Dashboard/JobAds/ManageApplications.jsx
@@ -0,0 +1,5 @@
+const ManageApplications = () => {
+  return <div>manage Appli</div>;
+};
+
+export default ManageApplications;
