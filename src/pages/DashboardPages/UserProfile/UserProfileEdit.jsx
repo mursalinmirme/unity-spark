cd2d1e4a94ee10ec7cb682e7ce6802f30f72755d
Rifazul <rifazul50@gmail.com>
2024-02-03 23:38:54 +0600

    user profile and user profile edit page problem remove

--- src/pages/DashboardPages/UserProfile/UserProfileEdit.jsx
+++ src/pages/DashboardPages/UserProfile/UserProfileEdit.jsx
@@ -14,6 +14,7 @@ const UserProfileEdit = () => {
   const { register, handleSubmit, control, reset } = useForm();
   const [updateLoading, setUpdateLoading] = useState(false);
   const { user } = useContext(AuthContext);
+  const { profileComplete } = useContext(AuthContext);
   const navigate = useNavigate();
   const axiosPublic = useAxiosPublic();
   // New Array
@@ -104,7 +105,7 @@ const UserProfileEdit = () => {
             <h2>{users?.name}</h2>
             <h3>{users?.email}</h3>
             <ProgressBar
-              completed={90}
+              completed={Math.ceil(profileComplete)}
               bgColor="#433ebe"
               height="12px"
               width="300px"
@@ -118,7 +119,8 @@ const UserProfileEdit = () => {
           <div>
             <Link
               to="/dashboard/userProfile"
-              className="edit_btn !text-red-500 hover:!text-white !border-red-600 hover:!border-red-600 hover:!bg-red-600">
+              className="edit_btn !text-red-500 hover:!text-white !border-red-600 hover:!border-red-600 hover:!bg-red-600"
+            >
               <span> X Cancel </span>
             </Link>
           </div>
@@ -150,7 +152,8 @@ const UserProfileEdit = () => {
               <span className="font-bold font-inter"> Your Photo : </span>
               <label
                 className="font-semibold w-full absolute bottom-0   text-white cursor-pointer font-inter text-base px-8 py-[8px] bg-primary rounded-md transition-all duration-500 text-[15px]"
-                htmlFor="user_photo">
+                htmlFor="user_photo"
+              >
                 <div className="flex justify-center items-center gap-4">
                   {" "}
                   {/* <img className="w-5 h-5" src={download_icon} alt="" />{" "} */}
@@ -256,7 +259,8 @@ const UserProfileEdit = () => {
 
             <select
               className="w-full py-2 mt-2 border rounded-lg pl-2"
-              {...register("gender")}>
+              {...register("gender")}
+            >
               <option> {users?.gender} </option>
               <option value="male">male</option>
               <option value="female">female</option>
@@ -310,7 +314,8 @@ const UserProfileEdit = () => {
 
             <select
               className="w-full py-2 mt-2 border rounded-lg pl-2"
-              {...register("preference")}>
+              {...register("preference")}
+            >
               <option> {users?.job_preference} </option>
               <option value="Remote">Remote</option>
               <option value="On-site">On-site</option>
@@ -330,7 +335,8 @@ const UserProfileEdit = () => {
 
             <select
               className="w-full py-2 mt-2 border rounded-lg pl-2"
-              {...register("time_preference")}>
+              {...register("time_preference")}
+            >
               <option> {users?.time_preference} </option>
               <option value="intern">intern</option>
               <option value="full-time">full-time</option>
