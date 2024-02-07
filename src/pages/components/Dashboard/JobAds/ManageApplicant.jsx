448307a46df6e6bd14850b0bab6495b6a3b81497
mursalinmirme <mursalinmir02@gmail.com>
2024-02-07 16:19:21 +0600

    I have polish the manage job applicationa and manage jobs applicatins page

--- src/pages/components/Dashboard/JobAds/ManageApplicant.jsx
+++ src/pages/components/Dashboard/JobAds/ManageApplicant.jsx
@@ -5,34 +5,41 @@ import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 // import { RxCross1 } from "react-icons/rx";
 // import { HiDotsVertical } from "react-icons/hi";
 // import { Link } from "react-router-dom";
-import useAxiosPublic from "../../../../hooks/useAxiosPublic";
-import ApplicationsCard from "./ApplicationsCard";
-import Swal from "sweetalert2";
 import { useQuery } from "@tanstack/react-query";
-import Loading from "../../Loading/Loading";
+import Swal from "sweetalert2";
+import useAxiosPublic from "../../../../hooks/useAxiosPublic";
+import ApplicantsCard from "../ApplicantsCard";
 
 const ManageApplicant = () => {
   const [totalPages, setToalPages] = useState(0);
   const [currentPage, setCurrentPage] = useState(0);
+  const [applicationId, setApplicationId] = useState(" ");
+
   // const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const axiosPublic = useAxiosPublic();
-  const {data: jobapplicantsNum = [] , refetch:refetchjobapplicantsNum} = useQuery({
-    queryKey: ['jobapplicantsNums', totalPages, currentPage],
-    queryFn: async () =>{
-    const res = await  axiosPublic.get("/job_applications_nums");
-    setToalPages(Math.ceil(res?.data?.total / 5))
-    return res?.data}
-    
-  })
-  console.log("total page num is", totalPages);
+  const { data: jobapplicantsNum = [], refetch: refetchjobapplicantsNum } =
+    useQuery({
+      queryKey: ["jobapplicantsNums", totalPages, currentPage],
+      queryFn: async () => {
+        const res = await axiosPublic.get("/job_applicants_nums");
+        setToalPages(Math.ceil(res?.data?.total / 6));
+        return res?.data;
+      },
+    });
   // fetch the applications by pagination
-  const {data: jobapplicants = [] , isFetching, refetch} = useQuery({
-    queryKey: ['jobapplicants', totalPages, currentPage],
-    queryFn: async () =>{
-    const res = await  axiosPublic.get(`/job_applications?skip=${currentPage * 5}`);
-    return res?.data}
-    
-  })
+  const {
+    data: jobapplicants = [],
+    isFetching,
+    refetch,
+  } = useQuery({
+    queryKey: ["jobapplicants", totalPages, currentPage],
+    queryFn: async () => {
+      const res = await axiosPublic.get(
+        `/job_applicants?skip=${currentPage * 6}`
+      );
+      return res?.data;
+    },
+  });
   // handle next btn pagination
   const handleRightPagi = () => {
     if (currentPage + 1 < totalPages) {
@@ -47,83 +54,118 @@ const ManageApplicant = () => {
     }
   };
 
-  const pagesArray = Array.from(
-    { length: totalPages },
-    (_, index) => index
-  );
+  const pagesArray = Array.from({ length: totalPages }, (_, index) => index);
   // delete operation
   const handleDelete = (id) => {
-    axiosPublic.delete(`/job_applications/${id}`)
-    .then(res =>{
-      console.log(res.data)
-      Swal.fire({
-    title: "Are you sure?",
-    text: "You won't be able to revert this!",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Yes, delete it!"
-  }).then((result) => {
-    if (result.isConfirmed) {
-      if(res.data.deletedCount > 0 ){
-        refetch()
-        Swal.fire({
-          title: "Deleted!",
-          text: "Article Deleted",
-          icon: "success"
-        });
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You want to delete this applicants!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosPublic
+          .delete(`/job_applications/${id}`)
+          .then((res) => {
+            console.log(res.data);
+            Swal.fire({
+              title: "Deleted!",
+              text: "Applicant Deleted successfully",
+              icon: "success",
+            });
+            refetch();
+          })
+          .catch((error) => {
+            console.log(error);
+          });
       }
-     
-    }
+    });
+  };
+
+  // update role
+
+  const handleUpdateRole = (value, appli_id) => {
+    console.log("new emaployee email is", value, appli_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You want to make this user as a employee",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, Sure",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosPublic
+          .put(`/users?email=${value}`)
+          .then((res) => {
+            console.log("user role update info is and next id is", res.data, appli_id);
+              axiosPublic
+              .put(`/application-status/${appli_id}`, { status: "Selected" })
+              .then((res) => {
+                Swal.fire({
+                title: "Make Employee Successfully",
+                text: `${value} is now Employee`,
+                icon: "success",
+                });
+                refetch();
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    });
+  };
+
+  const { data: applicationPreview } = useQuery({
+    queryKey: ["getIndivisulItems", applicationId],
+    enabled: !!applicationId,
+    queryFn: async () => {
+      const res = await axiosPublic.get(`/job_applications/${applicationId}`);
+      return res?.data;
+    },
   });
-      
-    })
-    .catch(error =>{
-      console.log(error)
-    })
-  
-    }
 
-    // update role 
+  console.log("Indivisual37", applicationPreview);
 
-   const handleUpdateRole = (value) => {
-      axiosPublic.put(`/users?email=${value?.email}`)
-      .then(res => {
-        if(res?.data?.modifiedCount > 0){
-          Swal.fire({
-            title: "Role Updated",
-            text: `${value?.email} is now Employee`,
-            icon: "success"
-          }).then((result) => {
-            if (result.isConfirmed) {
-              handleDelete(value._id);
-            }})
-          
-          
-        }
-      })
-      .catch(error => {
-        console.log(error)
-      })
-    }
-  
   // if(isFetching){
   //   return <Loading></Loading>
   // }
 
   return (
     <div className="py-10">
-      <div className="min-h-[460px] space-y-3">
-        {jobapplicants?.map((value) => (
-          <ApplicationsCard key={value._id} value={value} handleUpdateRole={handleUpdateRole} handleDelete={handleDelete}>
-
-          </ApplicationsCard>
-        ))}
-      </div>
+      {jobapplicants.length > 0 ? (
+        <div className="min-h-[460px] space-y-3">
+          {jobapplicants?.map((value) => (
+            <ApplicantsCard
+              key={value._id}
+              value={value}
+              handleUpdateRole={handleUpdateRole}
+              handleDelete={handleDelete}
+              setApplicationId={setApplicationId}
+              applicationPreview={applicationPreview}
+            ></ApplicantsCard>
+          ))}
+        </div>
+      ) : (
+        <div className="min-h-[460px] space-y-3 flex justify-center items-center">
+          <h4 className="text-xl font-semibold">
+            There has not job applications
+          </h4>
+        </div>
+      )}
 
       {/* pagination */}
-      <div className={`mt-10 ${jobapplicantsNum?.total > 5 ? 'block' : 'hidden'}`}>
+      <div
+        className={`mt-10 ${jobapplicantsNum?.total > 6 ? "block" : "hidden"}`}
+      >
         <div className={`flex justify-center`}>
           <div className={`join flex space-x-2`}>
             <button
