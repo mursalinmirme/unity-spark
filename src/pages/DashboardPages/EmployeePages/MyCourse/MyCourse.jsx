b94de298f2be5a3b4079d1316023583ba982d136
mdabdurrahman07 <jamilrahman2001@gmail.com>
2024-02-15 15:29:59 +0600

    trending course & my course responsive issue fixed

--- src/pages/DashboardPages/EmployeePages/MyCourse/MyCourse.jsx
+++ src/pages/DashboardPages/EmployeePages/MyCourse/MyCourse.jsx
@@ -8,7 +8,7 @@ const MyCourse = () => {
     
     return (
         <div className="my-5">
-         <div className="grid grid-cols-2 items-center gap-5">
+         <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-5">
          {
                 my_course?.map(info => <div key={info?._id} className="border-2 border-[#46A3E1] rounded-xl overflow-hidden h-[500px]">
                     <img src={info?.uniqueID?.image} alt="course-img" className="rounded-t-lg w-full overflow-hidden" />
@@ -21,14 +21,14 @@ const MyCourse = () => {
                                    <h1 className="text-lg font-medium text-[#46A3e1]">40 Lessons</h1>
                                 </div>
                                 <div className="bg-[#BBDDF4] px-2.5 flex items-center gap-2 rounded-lg">
-                                   <BsClock className="text-[#46A3e1] text-lg "></BsClock>
+                                   <BsClock className="text-[#46A3e1] text-lg"></BsClock>
                                    <h1 className="text-lg font-medium text-[#46A3e1]">240 Hour</h1>
                                 </div>
                                 
                             </div>
                        </div>    
                         <div>
-                        <Link to={`/mycourse/${info?.uniqueID?._id}`} className="px-6 py-3 bg-[#433EBE] text-white font-semibold text-xl rounded-xl">Continue</Link>
+                        <Link to={`/mycourse/${info?.uniqueID?._id}`} className="px-6 py-3  bg-[#433EBE] text-white font-semibold text-xl rounded-xl">Continue</Link>
                         </div>
                     </div>
                   </div>
