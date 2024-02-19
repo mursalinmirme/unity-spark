84fc7d0368ce3406a0fda33db2b87e98d076499a
mursalinmirme <mursalinmir02@gmail.com>
2024-02-19 20:31:55 +0600

    I have fixed all users, employees blog request pages tables text align issues

--- src/pages/DashboardPages/AdminPages/AllUsers/AllUsers.jsx
+++ src/pages/DashboardPages/AdminPages/AllUsers/AllUsers.jsx
@@ -17,11 +17,11 @@ const AllUsers = () => {
   return (
     <div>
       <div className="overflow-x-auto">
-        <h3 className="text-3xl font-semibold">All our users</h3>
-        <table className="table-no-border table mt-10">
+        <h3 className="text-2xl font-semibold">Our All Users</h3>
+        <table className="table-no-border table mt-5">
           {/* head */}
           <thead className="bg-[#726eec] text-white text-[18px] rounded-md text-center">
-            <tr className="text-center">
+            <tr className="text-left">
               <th>Serial</th>
               <th>Image</th>
               <th>Name</th>
@@ -29,7 +29,7 @@ const AllUsers = () => {
               <th>Role</th>
             </tr>
           </thead>
-          <tbody>
+          <tbody className="">
             {allUsers &&
               allUsers?.map((user, idx) => (
                 <AllUsersTable
