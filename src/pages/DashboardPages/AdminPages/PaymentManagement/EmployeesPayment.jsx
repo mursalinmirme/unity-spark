a735283c225c6a200f3e5ab2cd0dbabe73481f36
mizan-chowdhury <mizan1034@gmail.com>
2024-02-21 21:37:44 +0600

    table alignment issue fixed

--- src/pages/DashboardPages/AdminPages/PaymentManagement/EmployeesPayment.jsx
+++ src/pages/DashboardPages/AdminPages/PaymentManagement/EmployeesPayment.jsx
@@ -3,7 +3,7 @@ import EmployeePaymentTable from "./EmployeePaymentTable";
 import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 
 const EmployeesPayment = () => {
-  const axiosSecure = useAxiosSecure()
+  const axiosSecure = useAxiosSecure();
 
   const { data: employees = [] } = useQuery({
     queryKey: ["employees"],
@@ -16,10 +16,10 @@ const EmployeesPayment = () => {
   return (
     <div>
       <div>
-        <table className="table border-[#45A5E1] mt-10">
+        <table className="table mt-10">
           {/* head */}
-          <thead className="bg-[#45A5E1] text-white text-[18px] text-center">
-            <tr className="row-border border-2 border-[#45A5E1] rounded-lg">
+          <thead className="bg-[#45A5E1] text-white">
+            <tr className="row-border table-style">
               <th>Name</th>
               <th>Position</th>
               <th>Salary</th>
@@ -32,7 +32,8 @@ const EmployeesPayment = () => {
                 <EmployeePaymentTable
                   key={employee?._id}
                   employee={employee}
-                  idx={idx}></EmployeePaymentTable>
+                  idx={idx}
+                ></EmployeePaymentTable>
               ))}
           </tbody>
         </table>
