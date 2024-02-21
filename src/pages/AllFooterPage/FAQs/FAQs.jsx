a7d4d3081815f500b60effb5cc2ba7d6274d1815
Rifazul <rifazul50@gmail.com>
2024-02-21 23:18:51 +0600

    i have FAQ page design completed

--- src/pages/AllFooterPage/FAQs/FAQs.jsx
+++ src/pages/AllFooterPage/FAQs/FAQs.jsx
@@ -1,13 +1,59 @@
 const FAQs = () => {
   return (
-    <div className=" mt-10 mb-3">
+    <div className="mt-10 mb-3">
       <img
-        className="h-[500px] w-full"
-        src="https://cdn.rentechdigital.com/common_files/blogs/dotcom-blog/what-is-a-faq-page-1-designing-a-dynamic-faq-page-with-drag-and-drop-builder-dotcom-dotcom-blog-27-7-23.jpg"
+        className=" rounded-lg w-full"
+        src="https://i.ibb.co/9TKr4kF/FAQ-image.png"
         alt=""
       />
-      <h3> Hello Ever One</h3>
-      <p> This is FAQ Part </p>
+      <div className="py-10">
+        <h3 className="text-3xl font-bold font-inter">
+          {" "}
+          <span className="text-primary">Frequently</span>{" "}
+          <span className="border-b-4 border-primary">Asked </span> Question
+        </h3>
+        {/*  Just Question Part*/}
+        <div className="space-y-4 mt-10">
+          <div className="collapse collapse-arrow bg-base-200">
+            <input type="radio" name="my-accordion-2" defaultChecked />
+            <div className="collapse-title text-xl font-medium">
+              What is Regular License?
+            </div>
+            <div className="collapse-content">
+              <p>
+                In simple terms, Regular License means your end product is
+                distributed for free
+              </p>
+            </div>
+          </div>
+          <div className="collapse collapse-arrow bg-base-200">
+            <input type="radio" name="my-accordion-2" />
+            <div className="collapse-title text-xl font-medium">
+              What is Extended License?
+            </div>
+            <div className="collapse-content">
+              <p>
+                In simple terms, Extended License means your end product may be
+                sold or otherwise limited to paying customers.
+              </p>
+            </div>
+          </div>
+          <div className="collapse collapse-arrow bg-base-200">
+            <input type="radio" name="my-accordion-2" />
+            <div className="collapse-title text-xl font-medium">
+              What will you get?
+            </div>
+            <div className="collapse-content">
+              <p>
+                All HTML files, All Angular files, All Materialize CSS files,
+                CSS Files, SCSS Files, JS Files, Documentation, Reliable
+                Technical Support System, Lifetime Template Updates, Template
+                Documentation
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
