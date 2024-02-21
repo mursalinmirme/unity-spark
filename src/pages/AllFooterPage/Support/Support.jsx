75f87997195cb313738ec367ca89c13d5e5dc033
Rifazul <rifazul50@gmail.com>
2024-02-21 12:49:52 +0600

    create a support page and some custom text use

---
+++ src/pages/AllFooterPage/Support/Support.jsx
@@ -0,0 +1,19 @@
+const Support = () => {
+  return (
+    <div className="mt-10">
+      <div>
+        <h3 className="text-2xl font-inter font-bold my-2">
+          Welcome to Unity Spark Support{" "}
+        </h3>
+        <p className="text-slate-500  text-justify">
+          Welcome to [Your Company Name] Support At committed to providing
+          exceptional support to our valued clients. Whether you have questions
+          about our services, need assistance with technical issues, or want to
+          provide feedback, our dedicated support team is here to help.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Support;
