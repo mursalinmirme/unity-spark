d2dd9bfc25511a61c16f2158be47edef995885da
mursalinmirme <mursalinmir02@gmail.com>
2024-02-25 14:41:30 +0600

    I have cleaned the manage ads and manage application file with arrenge the code

--- src/pages/components/HomeComponents/newsletter/Newsletter.jsx
+++ src/pages/components/HomeComponents/newsletter/Newsletter.jsx
@@ -5,14 +5,21 @@ const Newsletter = () => {
         background: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.4)), url(${testimonialBg})`
     }
 
+    // handle subscribe newsletter system
+    const handleNewsLetter = (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        alert(email)
+    }
+
     return (
         <div className="newsletter">
             <div className="newsletter_container" style={newsBG}>
                 <div className="second_container">
                     <h2>Our Newsletter</h2>
                     <p>Subscribe our newsletter to get update from us</p>
-                    <form>
-                        <input type="email" placeholder="Enter Your Email...."  />
+                    <form onSubmit={handleNewsLetter}>
+                        <input name="email" type="email" placeholder="Enter Your Email...."  />
                         <button className="bg-primary text-white">Subscribe</button>
                     </form>
                 </div>
