2a8651831595e3675f37d81d5e4fe007cce91362
ashrafulrifaz <developerashrafulislamrifaz@gmail.com>
2024-02-28 17:20:21 +0600

    changed banner style

--- src/pages/components/HomeComponents/Banner/Banner.jsx
+++ src/pages/components/HomeComponents/Banner/Banner.jsx
@@ -1,22 +1,24 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import { Autoplay, Pagination } from "swiper/modules";
-import { useQuery } from "@tanstack/react-query";
-import Loading from "../../Loading/Loading";
-import axios from "axios";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import "swiper/css";
+// import "swiper/css/pagination";
+// import { Autoplay, Pagination } from "swiper/modules";
+// import { useQuery } from "@tanstack/react-query";
+// import Loading from "../../Loading/Loading";
+// import axios from "axios";
+import BannerImg from '../../../../assets/images/banner.gif'
+
 const Banner = () => {
-  const { isPending, data: banner } = useQuery({
-    queryKey: ["banner_info"],
-    queryFn: async () => {
-      const res = await axios.get("banner_info.json");
-      return res.data;
-    },
-  });
+  // const { isPending, data: banner } = useQuery({
+  //   queryKey: ["banner_info"],
+  //   queryFn: async () => {
+  //     const res = await axios.get("banner_info.json");
+  //     return res.data;
+  //   },
+  // });
 
   return (
     <div className="banner">
-      {isPending ? (
+      {/* {isPending ? (
         <Loading></Loading>
       ) : (
         <Swiper
@@ -49,7 +51,18 @@ const Banner = () => {
             </SwiperSlide>
           ))}
         </Swiper>
-      )}
+      )} */}
+      <div className="slide_container">
+        <div className="space-y-3 md:space-y-6">
+          <span>Hi there</span>
+          <h2>Elevate Your Workforce Management</h2>
+          <p>Enhance efficiency and engagement with our comprehensive HR solutions. Streamline processes, elevate workplace experience, and transform operations today.</p>
+          <button className="nbtn">Get Started Today</button>
+        </div>
+        <div>
+          <img src={BannerImg} className="w-full" alt="" />
+        </div>
+      </div>
     </div>
   );
 };
