8a50ce6e4efcf3636aeb94c3012b8f33d93c958e
ashrafulrifaz <developerashrafulislamrifaz@gmail.com>
2024-02-28 17:20:51 +0600

    changed banner styles

--- src/pages/components/HomeComponents/Banner/Banner.jsx
+++ src/pages/components/HomeComponents/Banner/Banner.jsx
@@ -1,57 +1,9 @@
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import "swiper/css";
-// import "swiper/css/pagination";
-// import { Autoplay, Pagination } from "swiper/modules";
-// import { useQuery } from "@tanstack/react-query";
-// import Loading from "../../Loading/Loading";
-// import axios from "axios";
 import BannerImg from '../../../../assets/images/banner.gif'
 
 const Banner = () => {
-  // const { isPending, data: banner } = useQuery({
-  //   queryKey: ["banner_info"],
-  //   queryFn: async () => {
-  //     const res = await axios.get("banner_info.json");
-  //     return res.data;
-  //   },
-  // });
 
   return (
     <div className="banner">
-      {/* {isPending ? (
-        <Loading></Loading>
-      ) : (
-        <Swiper
-          spaceBetween={30}
-          centeredSlides={true}
-          loop={true}
-          autoplay={{
-            delay: 3000,
-            disableOnInteraction: false,
-          }}
-          pagination={{
-            clickable: true,
-          }}
-          navigation={false}
-          modules={[Autoplay, Pagination]}
-        >
-          {banner?.map((ban, idx) => (
-            <SwiperSlide key={idx}>
-              <div className="slide_container">
-                <div className="space-y-3 md:space-y-6">
-                  <span>Hi there</span>
-                  <h2>{ban?.title}</h2>
-                  <p>{ban?.description}</p>
-                  <button className="nbtn">{ban?.button}</button>
-                </div>
-                <div>
-                  <img src={ban?.img} className="w-full" alt="" />
-                </div>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      )} */}
       <div className="slide_container">
         <div className="space-y-3 md:space-y-6">
           <span>Hi there</span>
