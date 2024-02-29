5065960cc57df516ad527ec2236afab96df29b91
Rifazul <rifazul50@gmail.com>
2024-02-29 20:36:45 +0600

    I have completed skeleton in the user dashboard interview page and some modified

--- src/pages/DashboardPages/UserPages/Interview/SkeletonInterview.jsx
+++ src/pages/DashboardPages/UserPages/Interview/SkeletonInterview.jsx
@@ -7,13 +7,13 @@ const SkeletonInterview = () => {
       </div>
       {/**Body Part Candidate and Interviewer */}
       <div className=" mt-5px  flex lg:flex-row flex-col justify-between ">
-        <div className=" w-full lg:border-r-2 border-gray-600 ">
-          <div className="flex justify-end p-2">
+        <div className="w-full lg:border-r-2 border-gray-600 ">
+          <div className="flex  justify-center lg:justify-end p-2 my-5">
             <p className="skeleton  w-28 h-[30px] bg-slate-400 rounded-none"></p>
           </div>
 
           <div className=" text-center mx-auto space-y-2">
-            <p className="skeleton w-16 h-16 rounded-full mx-auto bg-slate-500"></p>
+            <p className="skeleton w-16 h-16 rounded-full mx-auto bg-slate-400"></p>
             <h3 className="skeleton mx-auto w-64 h-[30px] bg-slate-300 rounded-none"></h3>
             <h3 className="skeleton mx-auto w-72 h-[30px] bg-slate-300 rounded-none"></h3>
           </div>
@@ -21,16 +21,21 @@ const SkeletonInterview = () => {
 
         {/**  Card2 */}
         <div className=" w-full ">
-          <div className=" p-2">
+          <div className="flex justify-center lg:justify-start p-2 my-5">
             <p className="skeleton  w-28 h-[30px] bg-slate-400 rounded-none"></p>
           </div>
 
           <div className=" text-center mx-auto space-y-2">
-            <p className="skeleton w-16 h-16 rounded-full mx-auto bg-slate-500"></p>
+            <p className="skeleton w-16 h-16 rounded-full mx-auto bg-slate-400"></p>
             <h3 className="skeleton mx-auto w-64 h-[30px] bg-slate-300 rounded-none"></h3>
             <h3 className="skeleton mx-auto w-72 h-[30px] bg-slate-300 rounded-none"></h3>
           </div>
         </div>
+      </div>
+
+      {/** Button */}
+      <div className="skeleton mx-auto w-40  h-[30px] bg-slate-300 rounded-md mt-20">
+        <button></button>
       </div>
     </div>
   );
