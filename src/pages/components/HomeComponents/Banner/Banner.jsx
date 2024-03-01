ceb69f4573ba3342c9ce9c6a5b19f923fd77bf90
nisakib07 <websakib07@gmail.com>
2024-03-01 10:00:05 +0600

    Banner fixed

--- src/pages/components/HomeComponents/Banner/Banner.jsx
+++ src/pages/components/HomeComponents/Banner/Banner.jsx
@@ -1,16 +1,21 @@
-import BannerImg from '../../../../assets/images/banner.gif'
+import BannerImg from "../../../../assets/images/banner.gif";
 
 const Banner = () => {
-
   return (
     <div className="banner">
       <div className="slide_container">
+        {/* Banner Text */}
         <div className="space-y-3 md:space-y-6">
           <span>Hi there</span>
           <h2>Elevate Your Workforce Management</h2>
-          <p>Enhance efficiency and engagement with our comprehensive HR solutions. Streamline processes, elevate workplace experience, and transform operations today.</p>
+          <p>
+            Enhance efficiency and engagement with our comprehensive HR
+            solutions. Streamline processes, elevate workplace experience, and
+            transform operations today.
+          </p>
           <button className="nbtn">Get Started Today</button>
         </div>
+        {/* Banner Image */}
         <div>
           <img src={BannerImg} className="w-full" alt="" />
         </div>
