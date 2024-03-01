fd9453692cc719cd25a4a5cf57327c121bafdd70
nisakib07 <websakib07@gmail.com>
2024-03-01 09:55:55 +0600

    Comment added

--- src/pages/DashboardPages/EmployeePages/SavedBlogs/SavedBlogs.jsx
+++ src/pages/DashboardPages/EmployeePages/SavedBlogs/SavedBlogs.jsx
@@ -23,6 +23,7 @@ const SavedBlogs = () => {
       {isFetching ? (
         <SavedBlogSkeleton></SavedBlogSkeleton>
       ) : (
+        // saved blogs part
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {SavedBlogs &&
             SavedBlogs?.map((singleBlog) => (
