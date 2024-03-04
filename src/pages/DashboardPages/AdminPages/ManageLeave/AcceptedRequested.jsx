8ec21ab66b4c699ba11d3fe650247eacc9ab17c9
nisakib07 <websakib07@gmail.com>
2024-03-05 00:06:11 +0600

    Accepted and rejected leave requests fixed

--- src/pages/DashboardPages/AdminPages/ManageLeave/AcceptedRequested.jsx
+++ src/pages/DashboardPages/AdminPages/ManageLeave/AcceptedRequested.jsx
@@ -3,28 +3,28 @@ import useAxiosPublic from "../../../../hooks/useAxiosPublic";
 import AcceptAndRejectLeaveSkeleton from "./AcceptAndRejectLeaveSkeleton";
 
 const AcceptedRequested = () => {
-    const axiosPublic = useAxiosPublic();
-    const { data: leaveRequests = [], isFetching } = useQuery({
-      queryKey: ["GetAllLeaveRequests"],
-      queryFn: async () => {
-        const result = await axiosPublic.get("/leaves-confirmed");
-        return result.data;
-      },
-    });
-    
-    if(isFetching){
-      return <AcceptAndRejectLeaveSkeleton></AcceptAndRejectLeaveSkeleton>
-    }
+  const axiosPublic = useAxiosPublic();
+  const { data: leaveRequests = [], isFetching } = useQuery({
+    queryKey: ["GetAllLeaveRequests"],
+    queryFn: async () => {
+      const result = await axiosPublic.get("/leaves-confirmed");
+      return result.data;
+    },
+  });
 
-    return (
-        <div className="mt-3 grid grid-cols-1 md:grid-cols-2 gap-5">
+  if (isFetching) {
+    return <AcceptAndRejectLeaveSkeleton></AcceptAndRejectLeaveSkeleton>;
+  }
+
+  return (
+    <div className="mt-3 grid grid-cols-1 md:grid-cols-2 gap-5">
       {leaveRequests?.map((request) => {
         return (
           <div className="border shadow-sm p-3.5 rounded-md" key={request?._id}>
             <div className="flex items-center justify-between border-b pb-2">
-              <div className="flex">
+              <div className="flex items-center">
                 <img
-                  className="w-14 h-14 rounded-full"
+                  className="w-12 h-12 rounded-full"
                   src={request?.user?.image}
                   alt=""
                 />
@@ -35,24 +35,23 @@ const AcceptedRequested = () => {
                   </p>
                 </div>
               </div>
-              <h3 className="font-bold text-lg mr-4">
+              <h3 className="font-semibold text-md mr-4">
                 {request?.numberOfDays} Days
               </h3>
             </div>
             <div className="mt-3">
-              <p className="text-lg font-medium">Subject: {request?.subject}</p>
-              <p className="font-normal text-base mt-1">
+              <p className="text-lg font-medium">
+                Subject: <span className="font-normal">{request?.subject}</span>
+              </p>
+              <p className="font-inter text-base mt-1">
                 {request?.leaveReason}
               </p>
-            </div>
-            <div className="mt-3 flex gap-5">
-            <div className="badge text-base px-4 h-9 rounded-md badge-ghost font-medium text-green-600">Confirmed</div>
             </div>
           </div>
         );
       })}
-        </div>
-    );
+    </div>
+  );
 };
 
 export default AcceptedRequested;
--- src/pages/DashboardPages/AdminPages/ManageLeave/RejectedRequestes.jsx
+++ src/pages/DashboardPages/AdminPages/ManageLeave/RejectedRequestes.jsx
@@ -3,54 +3,57 @@ import useAxiosPublic from "../../../../hooks/useAxiosPublic";
 import AcceptAndRejectLeaveSkeleton from "./AcceptAndRejectLeaveSkeleton";
 
 const RejectedRequestes = () => {
-    const axiosPublic = useAxiosPublic();
-    const { data: leaveRequests = [], isFetching } = useQuery({
-      queryKey: ["GetAllLeaveRequests"],
-      queryFn: async () => {
-        const result = await axiosPublic.get("/leaves-rejected");
-        return result.data;
-      },
-    });
-    if(isFetching){
-      return <AcceptAndRejectLeaveSkeleton></AcceptAndRejectLeaveSkeleton>
-    }
-    return (
-        <div className="mt-3 grid grid-cols-1 md:grid-cols-2 gap-5">
+  const axiosPublic = useAxiosPublic();
+  const { data: leaveRequests = [], isFetching } = useQuery({
+    queryKey: ["GetAllLeaveRequests"],
+    queryFn: async () => {
+      const result = await axiosPublic.get("/leaves-rejected");
+      return result.data;
+    },
+  });
+  if (isFetching) {
+    return <AcceptAndRejectLeaveSkeleton></AcceptAndRejectLeaveSkeleton>;
+  }
+  return (
+    <div className="mt-3 grid grid-cols-1 md:grid-cols-2 gap-5">
       {leaveRequests?.map((request) => {
         return (
-          <div className="border shadow-sm p-3.5 rounded-md flex flex-col h-full" key={request?._id}>
+          <div
+            className="border shadow-sm p-3.5 rounded-md flex flex-col h-full"
+            key={request?._id}>
             <div className="flex items-center justify-between border-b pb-2">
-              <div className="flex">
+              <div className="flex items-center">
                 <img
-                  className="w-14 h-14 rounded-full"
+                  className="w-12 h-12 rounded-full"
                   src={request?.user?.image}
                   alt=""
                 />
                 <div className="ml-2">
-                  <p className="font-semibold text-lg">{request?.user?.name}</p>
+                  <p className="font-semibold font-inter text-lg">
+                    {request?.user?.name}
+                  </p>
                   <p className="font-semibold text-base">
                     {request?.user?.position}
                   </p>
                 </div>
               </div>
-              <h3 className="font-bold text-lg mr-4">
+              <h3 className="font-semibold text-md mr-4">
                 {request?.numberOfDays} Days
               </h3>
             </div>
             <div className="mt-3">
-              <p className="text-lg font-medium">Subject: {request?.subject}</p>
-              <p className="font-normal text-base mt-1 flex-grow line-clamp-3">
-                { request?.leaveReason }
+              <p className="text-lg font-medium">
+                Subject: <span className="font-normal">{request?.subject}</span>
               </p>
-            </div>
-            <div className="mt-3 flex gap-5">
-              <div className="badge text-base px-4 h-9 rounded-md badge-ghost font-medium text-red-600">Rejected</div>
+              <p className="font-inter text-base mt-1 flex-grow line-clamp-3">
+                {request?.leaveReason}
+              </p>
             </div>
           </div>
         );
       })}
-        </div>
-    );
+    </div>
+  );
 };
 
 export default RejectedRequestes;
